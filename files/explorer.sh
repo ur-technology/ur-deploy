25c5ffc7d5902adb3c4a3479556d72e7a35741ce
John Reitano <jreitano@gmail.com>
2016-10-04 22:30:32 -0700

    changes

--- files/explorer.sh
+++ files/explorer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-TRANSACTION_RELAY_IP_ADDRESS=$(nslookup rpc-1.ur.technology | awk '/./{line=$0} END{print line}' | awk '{print $2}')
-echo "TRANSACTION_RELAY_IP_ADDRESS=$TRANSACTION_RELAY_IP_ADDRESS"
+
+nohup gur $BASE_GUR_OPTIONS $BOOTNODES_OPTION --rpcapi "db,personal,eth,net,web3" --rpccorsdomain="*" --rpc --rpcaddr="0.0.0.0" </dev/null >> ur_data/gur.log 2>&1 &
+
+PUBLIC_IP_ADDRESS=$(nslookup explorer-1.ur.technology | awk '/./{line=$0} END{print line}' | awk '{print $2}')
+echo "PUBLIC_IP_ADDRESS=$PUBLIC_IP_ADDRESS"
 set -eo pipefail
 cd explorer
 npm install bower
-sed -i "s/localhost/$TRANSACTION_RELAY_IP_ADDRESS/g" app/app.js
+sed -i "s/localhost/$PUBLIC_IP_ADDRESS/g" app/app.js
 sed -i 's/localhost/0.0.0.0/g' package.json
 echo "2 ******************************"
 grep start package.json
--- prepare-remote-hosts.sh
+++ prepare-remote-hosts.sh
@@ -2,11 +2,12 @@
 
 ALL_NODES="bootnode-1 bootnode-2 explorer-1 identifier-1 miner-1 rpc-1"
 
-ALL_NODES="bootnode-1"
+ALL_NODES="explorer-1"
 
 for h in $ALL_NODES
 do
   scp -F ssh_config -o StrictHostKeyChecking=no ~/.ssh/id_rsa_ur_capital $h:/root/.ssh/id_rsa
   scp -F ssh_config -o StrictHostKeyChecking=no ./prepare-local-host.sh $h:
   ssh -F ssh_config -o StrictHostKeyChecking=no $h ./prepare-local-host.sh $h
+  ssh -F ssh_config -o StrictHostKeyChecking=no $h "cd ur-deploy; docker logs \`hostname\`"
 done
