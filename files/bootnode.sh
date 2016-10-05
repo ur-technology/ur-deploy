f4fe9ad3c42322a4cb15ff3947cf023b466e1557
John Reitano <jreitano@gmail.com>
2016-10-04 19:41:59 -0700

    changes

--- files/bootnode.sh
+++ files/bootnode.sh
@@ -3,8 +3,8 @@
 . ./gur-options.sh
 set -eo pipefail
 if [[ "`uname -n`" == "bootnode-1" ]]; then
-  NODEKEYHEX=BOOTNODE1_KEYHEYHEX
+  NODEKEYHEX=$BOOTNODE1_KEYHEYHEX
 elif [[ "`uname -n`" == "bootnode-2" ]]; then
-  NODEKEYHEX=BOOTNODE2_KEYHEYHEX
+  NODEKEYHEX=$BOOTNODE2_KEYHEYHEX
 fi
 gur $BASE_GUR_OPTIONS --nodekeyhex $NODEKEYHEX
