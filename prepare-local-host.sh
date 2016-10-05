63aa47459be2474198821ca1a2152505487d1088
John Reitano <jreitano@gmail.com>
2016-10-04 17:48:16 -0700

    changes

--- prepare-local-host.sh
+++ prepare-local-host.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
-git clone git@github.com:ur-technology/ur-deploy.git
 rm -rf ur-deploy
+git clone git@github.com:ur-technology/ur-deploy.git
 cd ur-deploy
 echo https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m) >> file_name.txt
 curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
