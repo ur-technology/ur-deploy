1acb9c75427606f193d0bb662197ae936cfee497
John Reitano <jreitano@gmail.com>
2016-10-05 21:37:16 -0700

    changes

--- check.sh
+++ check.sh
@@ -6,5 +6,5 @@ ALL_NODES="bootnode-1 bootnode-2 explorer-1 miner-1"
 
 for h in $ALL_NODES
 do
-  ssh -F ssh_config -o StrictHostKeyChecking=no $h "cd ur-deploy; docker logs \`hostname\`"
+  ssh -F ssh_config -o StrictHostKeyChecking=no $h "cd ur-deploy; docker exec -it \`hostname\` ./attach.sh"
 done
