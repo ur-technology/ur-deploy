2e73b8674bf26d47908faa4038a6af8b184f9f13
John Reitano <jreitano@gmail.com>
2016-10-06 11:57:12 -0700

    changes

--- files/explorer.sh
+++ files/explorer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+. ./gur-options.sh
+set -eo pipefail
 nohup gur $BASE_GUR_OPTIONS $BOOTNODES_OPTION --rpcapi "db,personal,eth,net,web3" --rpccorsdomain="*" --rpc --rpcaddr="0.0.0.0" </dev/null > ~/ur_data/gur.log 2>&1 &
 
 PUBLIC_IP_ADDRESS=$(nslookup explorer-1.ur.technology | awk '/./{line=$0} END{print line}' | awk '{print $2}')
