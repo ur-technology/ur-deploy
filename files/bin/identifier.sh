ca39f10b4494655bfa25053209504dcea3ef4e6b
John Reitano <jreitano@gmail.com>
2016-10-07 21:56:17 -0700

    changes

--- files/bin/identifier.sh
+++ files/bin/identifier.sh
@@ -7,6 +7,7 @@ echo "pwd=`pwd`"
 echo "ls -l=`ls -l`"
 nohup gur $BASE_GUR_OPTIONS $BOOTNODES_OPTION --rpcapi "db,personal,eth,net,web3" --rpccorsdomain="*" --rpc --rpcaddr="127.0.0.1" </dev/null > ~/ur_data/gur.log 2>&1 &
 
+sudo chown -R deploy:deploy ur-money-queue-processor
 cd ur-money-queue-processor
 cp staging.env .env
 
