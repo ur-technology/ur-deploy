851dc4f1013a74fbec23916ae7f8629a8e84887e
John Reitano <jreitano@gmail.com>
2016-10-09 14:22:16 -0700

    changes

--- check-remote-hosts.sh
+++ check-remote-hosts.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-ALL_NODES="bootnode-1 bootnode-2 explorer-1 miner-1 transaction-relay-1 identifier-1"
+UR_ENV=$1
+if [[ "$UR_ENV" == "prod" ]]; then
+  ALL_NODES="bootnode-1 bootnode-2 explorer-1 miner-1 transaction-relay-1 identifier-1"
+elif [[ "$UR_ENV" == "dev" ]]; then
+  ALL_NODES="dev-bootnode-1 dev-bootnode-2 dev-explorer-1 dev-miner-1 dev-transaction-relay-1 dev-identifier-1"
+else
+  echo "Usage: $0 prod|dev"
+  exit 1
+fi
 
 for h in $ALL_NODES
 do
-  ssh -F ssh_config -o StrictHostKeyChecking=no root@$h "echo '';echo '***************'; echo $h:; docker logs $h"
+  BASE_HOSTNAME=$(echo $h | sed -e 's/^dev\-//')
+  ssh -F ssh_config -o StrictHostKeyChecking=no root@$h "echo '';echo '***************'; echo $h:; docker logs $BASE_HOSTNAME"
 done
--- prepare-remote-hosts.sh
+++ prepare-remote-hosts.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 UR_ENV=$1
-
 if [[ "$UR_ENV" == "prod" ]]; then
   ALL_NODES="bootnode-1 bootnode-2 explorer-1 miner-1 transaction-relay-1 identifier-1"
 elif [[ "$UR_ENV" == "dev" ]]; then
   ALL_NODES="dev-bootnode-1 dev-bootnode-2 dev-explorer-1 dev-miner-1 dev-transaction-relay-1 dev-identifier-1"
 else
-  echo "Usage: prepare-remote-hosts prod|dev"
+  echo "Usage: $0 prod|dev"
   exit 1
 fi
 
