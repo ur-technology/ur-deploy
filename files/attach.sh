dcb3a447e99ffcb482a610f1762257093e7855b2
John Reitano <jreitano@gmail.com>
2016-10-14 17:02:47 -0700

    changes

--- files/attach.sh
+++ files/attach.sh
@@ -2,4 +2,4 @@
 
 . files/gur-options.sh
 set -eo pipefail
-files/gur $BASE_GUR_OPTIONS attach ipc:/home/deploy/ur_data/geth.ipc
+files/gur $BASE_GUR_OPTIONS attach ipc:/home/deploy/ur_data/gur.ipc
--- files/stop-mining.sh
+++ files/stop-mining.sh
@@ -2,4 +2,4 @@
 
 . files/gur-options.sh
 set -eo pipefail
-files/gur $BASE_GUR_OPTIONS --exec "miner.stop();" attach ipc:/home/deploy/ur_data/geth.ipc
+files/gur $BASE_GUR_OPTIONS --exec "miner.stop();" attach ipc:/home/deploy/ur_data/gur.ipc
