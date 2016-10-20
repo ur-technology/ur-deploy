c084caf0b29e3f2fa1464d28a2b5c1d168de4f78
John Reitano <jreitano@gmail.com>
2016-10-20 15:24:46 -0700

    changes

--- files/identifier.sh
+++ files/identifier.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 echo "in $0: UR_ENV=$UR_ENV"
-echo "in $0: PRIVILEGED_UTI_OUTBOUND_PASSWORD=$PRIVILEGED_UTI_OUTBOUND_PASSWORD"
 
 . files/gur-options.sh
 set -eo pipefail
 
-nohup files/gur $BASE_GUR_OPTIONS $BOOTNODES_OPTION --rpcapi "db,personal,ur,net,web3" --rpccorsdomain="*" --rpc --rpcaddr="127.0.0.1" </dev/null > ~/ur_data/gur.log 2>&1 &
 
 rm -rf ~/ur-money-queue-processor
 cp -R files/ur-money-queue-processor ~/
+
+mkdir ~/ur_data/keystore
+cp ~/ur-money-queue-processor/keystore.$UR_ENV/* ~/ur_data/keystore/
+
+nohup files/gur $BASE_GUR_OPTIONS $BOOTNODES_OPTION --rpcapi "db,personal,ur,net,web3" --rpccorsdomain="*" --rpc --rpcaddr="127.0.0.1" </dev/null > ~/ur_data/gur.log 2>&1 &
+
 cd ur-money-queue-processor
-cp env.$UR_ENV .env
+cp env.node.$UR_ENV .env
 echo "PRIVILEGED_UTI_OUTBOUND_PASSWORD=$PRIVILEGED_UTI_OUTBOUND_PASSWORD" >> .env
 npm install
 typings install
