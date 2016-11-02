ebf70785530755915a216639066714e4f9466a4c
John Reitano <jreitano@gmail.com>
2016-11-02 14:33:53 -0700

    make it easier to restart node app

--- files/identifier.sh
+++ files/identifier.sh
@@ -18,4 +18,6 @@ echo "PRIVILEGED_UTI_OUTBOUND_PASSWORD=$PRIVILEGED_UTI_OUTBOUND_PASSWORD" >> .en
 npm install
 typings install
 # npm run-script debug
-npm start
+nohup npm start </dev/null > ~/ur-money-queue-processor.log 2>&1 &
+
+tail -f ~/ur-money-queue-processor.log
