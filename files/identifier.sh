7c5c4b84a8947f12d24ccc02190685855cb214e4
John Reitano <jreitano@gmail.com>
2016-11-02 15:10:24 -0700

    changes

--- files/identifier.sh
+++ files/identifier.sh
@@ -11,6 +11,7 @@ mkdir -p ~/ur_data/keystore
 cp ~/files/keystore.$UR_ENV/* ~/ur_data/keystore/
 
 nohup ~/files/gur $BASE_GUR_OPTIONS $BOOTNODES_OPTION --rpcapi "db,personal,ur,eth,net,web3" --rpccorsdomain="*" --rpc --rpcaddr="127.0.0.1" </dev/null > ~/ur_data/gur.log 2>&1 &
+sleep 30
 
 cd ~/ur-money-queue-processor
 cp env.node.$UR_ENV .env
