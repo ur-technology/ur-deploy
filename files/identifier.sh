3e2243eeb711e1fcd34ac14622c17df2ef98ba9c
John Reitano <jreitano@gmail.com>
2016-11-02 11:11:01 -0700

    fix typo

--- files/identifier.sh
+++ files/identifier.sh
@@ -8,7 +8,7 @@ set -eo pipefail
 rm -rf ~/ur-money-queue-processor
 cp -R ~/files/ur-money-queue-processor ~/
 mkdir -p ~/ur_data/keystore
-cp ~/files/keystore.$UR_DEV/* ~/ur_data/keystore/
+cp ~/files/keystore.$UR_ENV/* ~/ur_data/keystore/
 
 nohup ~/files/gur $BASE_GUR_OPTIONS $BOOTNODES_OPTION --rpcapi "db,personal,ur,eth,net,web3" --rpccorsdomain="*" --rpc --rpcaddr="127.0.0.1" </dev/null > ~/ur_data/gur.log 2>&1 &
 
