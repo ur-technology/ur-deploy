31f9280faf15aefab15a569c26719e5f49b9a939
John Reitano <jreitano@gmail.com>
2016-11-07 09:31:46 -0800

    changes

--- connect-to-rpc.sh
+++ connect-to-rpc.sh
@@ -1,1 +1,1 @@
-curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getBalance","params":["0x0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae", "latest"],"id":1}' 127.0.0.1:9595
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getBalance","params":["0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae", "latest"],"id":1}' 127.0.0.1:9595
