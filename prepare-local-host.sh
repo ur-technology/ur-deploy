5eb55b1fb2757a5dc4b31cba1ab23b7b161cc233
John Reitano <jreitano@gmail.com>
2016-11-10 20:46:40 -0800

    only ask user for passphrase on production, not development

--- prepare-local-host.sh
+++ prepare-local-host.sh
@@ -55,18 +55,19 @@ fi
 
 # prepare .env file for dockerfile
 cp env.dockerfile.$UR_ENV .env
-PRIVILEGED_UTI_OUTBOUND_PASSWORD=""
-if [[ "$BASE_HOSTNAME" == *"queue-processor"* || "$BASE_HOSTNAME" == *"identifier"* ]]; then
-  echo "Please enter passphrase for privileged UTI-outbound address [wQEqfsik6i3CspYqVdh]: "
-  read -s PRIVILEGED_UTI_OUTBOUND_PASSWORD
-  echo ""
-  if  [[ -z $PRIVILEGED_UTI_OUTBOUND_PASSWORD  ]]; then
-    PRIVILEGED_UTI_OUTBOUND_PASSWORD=wQEqfsik6i3CspYqVdh
+if [[ "$UR_ENV" == "prod" ]]; then
+  PRIVILEGED_UTI_OUTBOUND_PASSWORD=""
+  if [[ "$BASE_HOSTNAME" == *"queue-processor"* || "$BASE_HOSTNAME" == *"identifier"* ]]; then
+    echo "Please enter passphrase for privileged UTI-outbound address [wQEqfsik6i3CspYqVdh]: "
+    read -s PRIVILEGED_UTI_OUTBOUND_PASSWORD
+    echo ""
+    if  [[ -z $PRIVILEGED_UTI_OUTBOUND_PASSWORD  ]]; then
+      PRIVILEGED_UTI_OUTBOUND_PASSWORD=wQEqfsik6i3CspYqVdh
+    fi
   fi
+  PRIVILEGED_UTI_OUTBOUND_PASSWORD=$(echo $PRIVILEGED_UTI_OUTBOUND_PASSWORD | sed -e 's/ /\-/g')
+  echo "PRIVILEGED_UTI_OUTBOUND_PASSWORD=$PRIVILEGED_UTI_OUTBOUND_PASSWORD" >> .env
 fi
-PRIVILEGED_UTI_OUTBOUND_PASSWORD=$(echo $PRIVILEGED_UTI_OUTBOUND_PASSWORD | sed -e 's/ /\-/g')
-echo "PRIVILEGED_UTI_OUTBOUND_PASSWORD=$PRIVILEGED_UTI_OUTBOUND_PASSWORD" >> .env
-
 apt-get install -y wget unzip
 wget --quiet https://github.com/ur-technology/go-ur/releases/download/UR-v0.0.1-alpha/gur-linux-amd64.zip && \
   unzip -d files gur-linux-amd64.zip gur
