c7782fa1a1b09d664c92e1727d967373f5629dc7
John Reitano <jreitano@gmail.com>
2016-12-07 12:00:23 -0800

    start using production/staging instead of prod/dev

--- files/transaction-relay.sh
+++ files/transaction-relay.sh
@@ -12,6 +12,11 @@ sleep 60
 cd ~/gur-https-proxy
 npm install
 typings install
+if [[ "$UR_ENV" == "prod" || "$UR_ENV" == "production" ]]; then
+  cp env.production .env
+else
+  cp env.staging .env
+fi
 # npm run-script debug
 touch ~/gur-https-proxy.log
 nohup npm start </dev/null >> ~/gur-https-proxy.log 2>&1 &
