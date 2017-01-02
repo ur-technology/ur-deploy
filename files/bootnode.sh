fef5ab46def1a08f425604e8c14d9057f0e49256
John Reitano <jreitano@gmail.com>
2017-01-02 14:35:18 -0800

    add third bootnode

--- files/bootnode.sh
+++ files/bootnode.sh
@@ -9,12 +9,13 @@ if [[ "$BASE_HOSTNAME" == "bootnode-1" ]]; then
   NODEKEYHEX=$BOOTNODE_1_NODEKEYHEX
 elif [[ "$BASE_HOSTNAME" == "bootnode-2" ]]; then
   NODEKEYHEX=$BOOTNODE_2_NODEKEYHEX
+elif [[ "$BASE_HOSTNAME" == "bootnode-3" ]]; then
+  NODEKEYHEX=$BOOTNODE_3_NODEKEYHEX
 else
-  NODEKEYHEX=$BOOTNODE_2_NODEKEYHEX
   echo "ERROR - UNEXPECTED BOOTNODE $BASE_HOSTNAME"
 fi
 
 touch ~/ur_data/gur.log
 # nohup ~/files/gur $BASE_GUR_OPTIONS --nodekeyhex $NODEKEYHEX </dev/null >> ~/ur_data/gur.log 2>&1 &
-nohup ~/files/gur $BASE_GUR_OPTIONS --nodiscover --maxpeers 5 </dev/null >> ~/ur_data/gur.log 2>&1 &
+nohup ~/files/gur $BASE_GUR_OPTIONS --nodekeyhex $NODEKEYHEX --nodiscover --maxpeers 5 </dev/null >> ~/ur_data/gur.log 2>&1 &
 tail -f ~/ur_data/gur.log
