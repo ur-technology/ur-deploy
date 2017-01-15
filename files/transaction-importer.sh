a1167d67e2fe943d1b4b534a35c8df0e3e95ef12
John Reitano <jreitano@gmail.com>
2017-01-15 00:45:05 -0800

    changes

--- files/transaction-importer.sh
+++ files/transaction-importer.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+echo "here 0"
 echo "in $0: UR_ENV=$UR_ENV"
 
 . files/gur-options.sh
 set -eo pipefail
 
 nohup ~/files/gur $BASE_GUR_OPTIONS $BOOTNODES_OPTION --rpcapi "db,personal,ur,eth,net,web3" --rpccorsdomain="*" --rpc --rpcaddr="127.0.0.1" </dev/null > ~/ur_data/gur.log 2>&1 &
-sleep 5
+sleep 30
+echo "here 1"
 
 rm -rf ~/ur-money-queue-processor
 cp -R ~/files/ur-money-queue-processor ~/
@@ -16,6 +18,7 @@ if [[ "$UR_ENV" == "production" ]]; then
 else
   cp env.transaction-importer.staging .env
 fi
+echo "here 2"
 
 npm install
 typings install
