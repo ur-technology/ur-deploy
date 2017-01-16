e6d7f74d643ba3cbb50b3661d5b5383d9a7baa9b
John Reitano <jreitano@gmail.com>
2017-01-15 18:46:23 -0800

    changes

---
+++ files/phone-auth.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "in $0: UR_ENV=$UR_ENV"
+
+set -eo pipefail
+
+rm -rf ~/ur-money-queue-processor
+cp -R ~/files/ur-money-queue-processor ~/
+cd ~/ur-money-queue-processor
+if [[ "$UR_ENV" == "production" ]]; then
+  cp env.phone-auth.production .env
+else
+  cp env.phone-auth.staging .env
+fi
+
+npm install
+typings install
+
+LOG=~/ur-money-queue-processor/ur-money-queue-processor.log
+touch $LOG
+nohup npm start </dev/null >> $LOG 2>&1 &
+tail -f $LOG
